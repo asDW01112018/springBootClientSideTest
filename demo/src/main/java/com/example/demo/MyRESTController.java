adb69ae8993bc7d798dd73d2033ddd932f3f4516
ASHUTOSH RANJAN SINHA <40268703+asDW01112018@users.noreply.github.com>
2018-11-02 13:34:05 +0530

    Add files via upload

--- demo/src/main/java/com/example/demo/MyRESTController.java
+++ demo/src/main/java/com/example/demo/MyRESTController.java
@@ -6,17 +6,38 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import OrderDetails.orderDetails;
+
 @RestController
 public class MyRESTController {
 
+	public orderDetails orderdetails=new orderDetails();
 	/**
 	 * This returns Hello World string
 	 * 
 	 * @return
 	 */
-	@RequestMapping("/login")
-	public String helloWithParams(@RequestParam(value = "username", defaultValue = "World")String name) {
-		System.out.println("Request recived"+name);
-		return "Hello " + name;
+	@RequestMapping("/create")
+	public String createWithParams(@RequestParam(value = "customerName", defaultValue = "Not Found")String customerName,@RequestParam(value = "date", defaultValue = "Not Found")String date,@RequestParam(value = "place", defaultValue = "Not Found")String place) {
+		System.out.println("Customer Name"+customerName);
+		System.out.println("Date"+date);
+		System.out.println("place"+place);
+		//create(id,customerName,date,place) return : succesful or unsuccesful  type:boolean
+		return "Hello " + customerName + date +place;
+	}
+	@RequestMapping("/update")
+	public String updateWithParams(@RequestParam(value = "customerName", defaultValue = "Not Found")String customerName,@RequestParam(value = "date", defaultValue = "Not Found")String date,@RequestParam(value = "place", defaultValue = "Not Found")String place) {
+		//update(id,customerName,date,place) return : succesful or unsuccesful  type:boolean
+		return "update " + customerName + date +place;
+	}
+	@RequestMapping("/retrive")
+	public String retriveWithParams(@RequestParam(value = "id", defaultValue = "")int id) {
+		//retrive(id) return : row with id type:String  
+		return "retrive ";
+	}
+	@RequestMapping("/delete")
+	public String deleteWithParams(@RequestParam(value = "id", defaultValue = "")int id) {
+		//delete(id)  return : succesful or unsuccesful  type:boolean
+		return "delete";
 	}
 }
