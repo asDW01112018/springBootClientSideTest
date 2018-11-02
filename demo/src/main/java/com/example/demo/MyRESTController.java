763e3f0c61e884d38b2a9f3758dd783ce0a36dfb
ASHUTOSH RANJAN SINHA <40268703+asDW01112018@users.noreply.github.com>
2018-11-02 16:05:07 +0530

    Add files via upload

--- demo/src/main/java/com/example/demo/MyRESTController.java
+++ demo/src/main/java/com/example/demo/MyRESTController.java
@@ -17,26 +17,29 @@ public class MyRESTController {
 	 * 
 	 * @return
 	 */
-	@RequestMapping("/create")
+	@RequestMapping(value="/request", method=RequestMethod.POST)
 	public String createWithParams(@RequestParam(value = "customerName", defaultValue = "Not Found")String customerName,@RequestParam(value = "date", defaultValue = "Not Found")String date,@RequestParam(value = "place", defaultValue = "Not Found")String place) {
 		System.out.println("Customer Name"+customerName);
-		System.out.println("Date"+date);
-		System.out.println("place"+place);
+		System.out.println("Date" + date);
+		System.out.println("place" + place);
 		//create(id,customerName,date,place) return : succesful or unsuccesful  type:boolean
 		return "Hello " + customerName + date +place;
 	}
-	@RequestMapping("/update")
+	@RequestMapping(value="/request", method=RequestMethod.PUT)
 	public String updateWithParams(@RequestParam(value = "customerName", defaultValue = "Not Found")String customerName,@RequestParam(value = "date", defaultValue = "Not Found")String date,@RequestParam(value = "place", defaultValue = "Not Found")String place) {
+		System.out.println("PUT:");
 		//update(id,customerName,date,place) return : succesful or unsuccesful  type:boolean
 		return "update " + customerName + date +place;
 	}
-	@RequestMapping("/retrive")
-	public String retriveWithParams(@RequestParam(value = "id", defaultValue = "")int id) {
+	@RequestMapping(value="/request", method=RequestMethod.GET)
+	public String retriveWithParams(@RequestParam(value = "id", defaultValue = "Not Found")String id) {
+		System.out.println("GET:");
 		//retrive(id) return : row with id type:String  
 		return "retrive ";
 	}
-	@RequestMapping("/delete")
-	public String deleteWithParams(@RequestParam(value = "id", defaultValue = "")int id) {
+	@RequestMapping(value="/request", method=RequestMethod.DELETE)
+	public String deleteWithParams(@RequestParam(value = "id", defaultValue = "")String id) {
+		System.out.println("DELETE:");
 		//delete(id)  return : succesful or unsuccesful  type:boolean
 		return "delete";
 	}
